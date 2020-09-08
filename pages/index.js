73410c7a097ee19d8f6f69e28fd9c646cae41000
Ryan Walters <rwalters@eb.com>
2020-09-08 14:56:11 -0500

    add canada; fix navbar; add count to navbar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { CheckBoxOutlineBlank, CheckBoxOutlined } from '@material-ui/icons';
 import Head from 'next/head';
 import React, { useReducer } from 'react';
-import { Container, ListGroup, ListGroupItem, Navbar, Table, UncontrolledAlert } from 'reactstrap';
-import states from '../utils/states.json';
+import { Badge, Container, ListGroup, ListGroupItem, Navbar, Table, UncontrolledAlert } from 'reactstrap';
+import states from '../utils/us-states.json';
+import provinces from '../utils/ca-provinces.json';
 
 const initialState = [];
 const ADD_STATE = 'ADD_STATE';
@@ -20,11 +21,11 @@ function reducer(state, { type, payload: abbreviation }) {
 }
 
 export default function Home() {
-  const [activeStates, dispatch] = useReducer(reducer, initialState);
+  const [activeTerritories, dispatch] = useReducer(reducer, initialState);
 
   function handleStateClick(abbreviation) {
     dispatch({
-      type: activeStates.includes(abbreviation) ? REMOVE_STATE : ADD_STATE,
+      type: activeTerritories.includes(abbreviation) ? REMOVE_STATE : ADD_STATE,
       payload: abbreviation,
     });
   }
@@ -32,44 +33,53 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Play the License Plate Game Online</title>
+        <title>Play the License Plate Game Online for Free</title>
+        <meta
+          name="description"
+          content="As you spot license plates from different states throughout your trip, check the box for the state you saw!"
+        />
       </Head>
-      <Navbar color="primary">
-        <div className="lead">License Plate Game!</div>
+      <Navbar color="primary" fixed="top" className="shadow">
+        <h1 className="lead mb-0">License Plate Game!</h1>
+        <div
+          className="rounded-circle bg-info text-center align-items-center justify-content-center"
+          style={{ height: '1.75em', width: '1.75em' }}
+        >
+          {activeTerritories.length}
+        </div>
       </Navbar>
       <Container>
         <UncontrolledAlert className="my-3">
           <div className="lead border-bottom mb-2 pb-2">How to play</div>
-          As you spot license plates from different states (plus DC), check the box for the state you saw!
+          As you spot license plates from different states and provinces during your trip, check the box for the state
+          you saw!
         </UncontrolledAlert>
-        <Table>
-          <thead>
-            <tr>
-              <th>Found</th>
-              <th>Remaining</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="lead">
-              <td>{activeStates.length}</td>
-              <td>{states.length - activeStates.length}</td>
-            </tr>
-          </tbody>
-        </Table>
-        <div className="font-weight-bold border-bottom border-dark pb-3 mb-3">Select State</div>
+        <div className="font-weight-bold border-bottom border-dark pb-3 mb-3 mt-4">United States</div>
         <ListGroup>
           {states.map(({ name, abbreviation }) => (
             <ListGroupItem
-              active={activeStates.includes(abbreviation)}
+              active={activeTerritories.includes(abbreviation)}
               onClick={() => handleStateClick(abbreviation)}
               className="d-flex align-items-center"
               key={name}
             >
-              {activeStates.includes(abbreviation) ? <CheckBoxOutlined /> : <CheckBoxOutlineBlank />}
+              {activeTerritories.includes(abbreviation) ? <CheckBoxOutlined /> : <CheckBoxOutlineBlank />}
               <span className="ml-3">{name}</span>
             </ListGroupItem>
           ))}
         </ListGroup>
+        <div className="font-weight-bold border-bottom border-dark pb-3 mb-3 mt-4">Canada</div>
+        {provinces.map(({ name, abbreviation }) => (
+          <ListGroupItem
+            active={activeTerritories.includes(abbreviation)}
+            onClick={() => handleStateClick(abbreviation)}
+            className="d-flex align-items-center"
+            key={name}
+          >
+            {activeTerritories.includes(abbreviation) ? <CheckBoxOutlined /> : <CheckBoxOutlineBlank />}
+            <span className="ml-3">{name}</span>
+          </ListGroupItem>
+        ))}
       </Container>
       <footer className="bg-dark p-3 mt-3">
         &copy; 2020 <a href="https://ryanwalters.dev">Ryan Walters</a>
