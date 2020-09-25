9cdf929794177de694e834a2f5f97e6974395765
Ryan Walters <shout at ryanwalters dot co>
2020-09-25 18:42:41 -0500

    useMultiplayer: change parameter signature to accept named parameters joinGameId, onDispatch; add ping interval; return sendMessage method; handle dispatch action

--- hooks/useMultiplayer.js
+++ hooks/useMultiplayer.js
@@ -1,11 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-export default function useMultiplayer() {
+export default function useMultiplayer({ joinGameId, onDispatch }) {
   const [websocket, setWebsocket] = useState(null);
   const [error, setError] = useState();
-  const [gameId, setGameId] = useState();
+  const [gameId, setGameId] = useState(joinGameId);
+
+  const createGame = useCallback(() => {
+    if (!websocket) {
+      setWebsocket(new WebSocket('wss://7esjaw2uci.execute-api.us-east-1.amazonaws.com/dev/'));
+    }
+  }, [websocket]);
+
+  const sendMessage = useCallback(
+    (payload) => {
+      try {
+        websocket?.send(JSON.stringify(payload));
+      } catch (err) {
+        // send this error somewhere
+      }
+    },
+    [websocket]
+  );
+
+  function disconnect() {
+    websocket?.close();
+  }
+
+  // Automatically create and join a game if 'joinGameId' is passed
 
   useEffect(() => {
+    if (joinGameId) {
+      createGame();
+    }
+  }, [createGame, joinGameId]);
+
+  useEffect(() => {
+    let interval;
+
     if (websocket) {
       // Create message listener
 
@@ -21,21 +52,33 @@ export default function useMultiplayer() {
             break;
           }
 
+          case 'dispatch': {
+            onDispatch(parsedData.payload);
+            break;
+          }
+
           default:
         }
-
-        console.log('message', parsedData);
       };
 
       // Fetch or join game id after opening the connection
 
       websocket.onopen = () => {
-        websocket.send(
-          JSON.stringify({
-            action: 'getGameId',
-          })
-        );
+        if (!joinGameId) {
+          sendMessage({ action: 'getGameId' });
+        } else {
+          sendMessage({
+            action: 'updateGameId',
+            gameId: joinGameId,
+          });
+        }
       };
+
+      // Send keep-alive ping every 6 min (automatically times out after 7 min)
+
+      interval = setInterval(() => {
+        sendMessage({ action: 'ping' });
+      }, 1000 * 60 * 6);
 
       // Disconnect
 
@@ -51,22 +94,16 @@ export default function useMultiplayer() {
 
     // Disconnect on unmount
 
-    return () => websocket?.close();
-  }, [websocket]);
-
-  function createGame() {
-    if (!websocket) {
-      setWebsocket(new WebSocket('wss://7esjaw2uci.execute-api.us-east-1.amazonaws.com/dev/'));
-    }
-  }
-
-  function disconnect() {
-    websocket?.close();
-  }
+    return () => {
+      websocket?.close();
+      clearInterval(interval);
+    };
+  }, [joinGameId, sendMessage, websocket]);
 
   return {
     createGame,
     disconnect,
+    sendMessage,
     error,
     gameId,
     isConnected: websocket?.readyState === 1, // WebSocket.OPEN
